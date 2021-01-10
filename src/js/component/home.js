5671b86642f58dd1983162c790257981614f9107
Outdoor-Dev <saltairpix@gmail.com>
2021-01-10 12:05:02 +0000

    update

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,19 +6,36 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<section>
+				<img
+					className="video"
+					src="https://wallpaperaccess.com/full/17515.jpg"
+				/>
+
+				<img className="mountain" src="mountain.png" id="mountain" />
+				<img className="lake" src="mountLeft.png" id="mountLeft" />
+				<img
+					className="mountRight"
+					src="mountRight.png"
+					id="mountRight"
+				/>
+
+				<div className="typewriter">
+					<h2 id="text">Outdoor Dev</h2>
+				</div>
+			</section>
+
+			<div className="typewriter d-flex justify-content-center">
+				<h1>Always Creating</h1>
+			</div>
+			<div className="buttons d-flex justify-content-center ">
+				<button className="btn first text-white mr-5">ABOUT</button>
+				<button className="btn second text-white mr-5">
+					PORTFOLIO
+				</button>
+				<button className="btn third text-white">CONTACT</button>
+			</div>
+		</>
 	);
 }
